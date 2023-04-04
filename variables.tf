8fde2126556f255e74d56f3b6f25386ccfda231c
Ethan Ho <ethan.j.ho-1@ou.edu>
2023-04-03 21:40:13 -0500

    Add descriptions to variables

--- variables.tf
+++ variables.tf
@@ -1,29 +1,36 @@
 variable "proxmox_api_url" {
-  type = string
+  description = "Proxmox API URL"
+  type        = string
 }
 
 variable "proxmox_api_token_id" {
-  type = string
+  description = "Proxmox API token ID"
+  type        = string
 }
 
 variable "proxmox_api_token_secret" {
-  type = string
+  description = "Proxmox API token secret"
+  type        = string
 }
 
 variable "proxmox_host" {
-  type = string
+  description = "Proxmox host to deploy VMs to"
+  type        = string
 }
 
 variable "vm_template_name" {
-  type = string
+  description = "Name of the VM template to clone"
+  type        = string
 }
 
 variable "ci_user" {
-  type = string
+  description = "Username for the cloud-init user"
+  type        = string
 }
 
 variable "ci_password" {
-  type = string
+  description = "Password for the cloud-init user"
+  type        = string
 }
 
 variable "physical_disks" {
