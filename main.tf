e683ed77c8ea62473797d166e6a9fcd47050665e
Ethan Ho <ethan.j.ho-1@ou.edu>
2023-04-03 21:21:23 -0500

    Format files

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 resource "proxmox_vm_qemu" "virtual_machines" {
-  count = 1
-  name = "benchmark-${count.index + 1}"
-  desc = "Storage benchmarking VM"
+  count       = 1
+  name        = "benchmark-${count.index + 1}"
+  desc        = "Storage benchmarking VM"
   target_node = var.proxmox_host
-  
+
   clone = var.vm_template_name
-  
+
   # VM System Settings
   agent = 1
-  
+
   # VM Basic Settings
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 2048
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 2048
   bootdisk = "scsi0"
-  
+
   # VM Network Settings
   network {
     bridge = "vmbr0"
@@ -25,10 +25,10 @@ resource "proxmox_vm_qemu" "virtual_machines" {
 
   disk {
     storage = "local-lvm"
-    type = "scsi"
-    size = "10G"
+    type    = "scsi"
+    size    = "2252M"
   }
-  
+
   # VM Cloud-Init Settings
   os_type    = "cloud-init"
   ciuser     = var.ci_user
--- providers.tf
+++ providers.tf
@@ -12,11 +12,11 @@ provider "proxmox" {
   pm_api_url = var.proxmox_api_url
 
   # Generate the following in Proxmox and set them in credentials.tfvars
-  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_id     = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
 
   # Leave insecure unless SSL is sorted out
   pm_tls_insecure = true
-  
+
   pm_debug = true
 }
